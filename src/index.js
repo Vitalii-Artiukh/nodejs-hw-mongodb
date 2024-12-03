a9d3d7eb147a9d1011550002b333d97c40902b15
Vitalii Artiukh <radial_car@yahoo.com>
2024-12-03 22:33:17 +0200

    in-processed

--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
-import { nodeJsModuleTwo } from '../learning/nodeJsModuleTwo.js';
+import { initMongoDB } from './db/initMongoDB.js';
 import { setupServer } from './server.js';
-import express from 'express';
 
-// console.log(setupServer());
-const app = express();
+const bootstrap = async () => {
+  await initMongoDB();
+  setupServer();
+};
 
-const PORT = 3000;
-
-// Middleware для логування часу запиту
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
-
-// Вбудований у express middleware для обробки (парсингу)
-// JSON - даних у запитах
-// наприклад, у запитах POST або PATCH
-app.use(express.json());
-
-// Маршрут для обробки GET-запитів на '/'
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello Hello',
-  });
-});
-
-// Middleware для обробких помилок (приймає 4 аргументи)
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: 'Something went wrong' });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is runing on port ${PORT}`);
-});
-
-// console.log(express);
+// bootstrap();
--- src/server.js
+++ src/server.js
@@ -1,3 +1,50 @@
-export const setupServer = (str) => {
-  return str;
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello Hello',
+    });
+  });
+
+  // app.use((req, res, next) => {
+  //   console.log(`Time: ${new Date().toLocaleString()}`);
+  //   next();
+  // });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is runing on port ${PORT}`);
+  });
 };
