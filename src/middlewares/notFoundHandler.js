8871db6a362867809b3b2cc76fc0aeeb81eb1f43
Vitalii Artiukh <radial_car@yahoo.com>
2024-12-11 12:15:21 +0200

    errors-fix

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,20 @@
-export const notFoundHandler = (req, res, next) => {
+import { HttpError } from 'http-errors';
+
+export const notFoundHandler = (error, req, res, next) => {
+  if (error instanceof HttpError) {
+    res.status(error.status).json({
+      status: error.status,
+      data: error,
+    });
+    return;
+  }
+
   res.status(404).json({
-    message: 'Not found',
+    status: 404,
+    message: 'Route not found',
+    data: error.message,
   });
+  // res.status(404).json({
+  //   message: 'Not found',
+  // });
 };
