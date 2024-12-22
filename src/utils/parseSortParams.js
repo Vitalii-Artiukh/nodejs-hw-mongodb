35628c7143537929c0c2147fcc828bee53da072c
Vitalii Artiukh <radial_car@yahoo.com>
2024-12-22 12:31:53 +0200

    fixed-hw-04

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -2,7 +2,8 @@ import { SORT_ORDER, sortByList } from '../constants/contacts.js';
 
 const parseSortOrder = (sortOrder) => {
   const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
-  if (isKnownOrder) return SORT_ORDER.ASC;
+  if (isKnownOrder) return sortOrder;
+  return SORT_ORDER.ASC;
 };
 
 const parseSortBy = (sortBy) => {
